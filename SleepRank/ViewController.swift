5f0a9e60bf62dde6a2d4f2a136b18d8302a233e0
wei170 <guocheng@guochengwei.com>
2016-11-05 02:40:25 -0500

    Start hacking

---
+++ SleepRank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepRank
+//
+//  Created by Walter Wei on 11/5/16.
+//  Copyright © 2016 SleepRank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SleepRankTests/SleepRankTests.swift
@@ -0,0 +1,36 @@
+//
+//  SleepRankTests.swift
+//  SleepRankTests
+//
+//  Created by Walter Wei on 11/5/16.
+//  Copyright © 2016 SleepRank. All rights reserved.
+//
+
+import XCTest
+@testable import SleepRank
+
+class SleepRankTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
